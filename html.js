436a92d3a75de5c51bad5e95288b0d17dcaddd07
em4321 <145388606+em4321@users.noreply.github.com>
2024-01-05 21:54:22 +0000

    Add files via upload

--- html.js
+++ html.js
@@ -10,7 +10,27 @@ export function getHtml(
   array0_temp,
   array0_feels_like,
   array0_humidity,
-
+  //Array2 data
+  array2_datetime,
+  array2_weather,
+  array2_icon,
+  array2_temp,
+  array2_feels_like,
+  array2_humidity,
+  //Array4 data
+  array4_datetime,
+  array4_weather,
+  array4_icon,
+  array4_temp,
+  array4_feels_like,
+  array4_humidity,
+  //Array4 data
+  array6_datetime,
+  array6_weather,
+  array6_icon,
+  array6_temp,
+  array6_feels_like,
+  array6_humidity,
   //Array8 data
   array8_datetime,
   array8_weather,
@@ -18,6 +38,27 @@ export function getHtml(
   array8_temp,
   array8_feels_like,
   array8_humidity,
+  //Array10 data
+  array10_datetime,
+  array10_weather,
+  array10_icon,
+  array10_temp,
+  array10_feels_like,
+  array10_humidity,
+  //Array12 data
+  array12_datetime,
+  array12_weather,
+  array12_icon,
+  array12_temp,
+  array12_feels_like,
+  array12_humidity,
+  //Array16 data
+  array14_datetime,
+  array14_weather,
+  array14_icon,
+  array14_temp,
+  array14_feels_like,
+  array14_humidity,
   //Array16 data
   array16_datetime,
   array16_weather,
@@ -25,6 +66,27 @@ export function getHtml(
   array16_temp,
   array16_feels_like,
   array16_humidity,
+  //Array18 data
+  array18_datetime,
+  array18_weather,
+  array18_icon,
+  array18_temp,
+  array18_feels_like,
+  array18_humidity,
+  //Array20 data
+  array20_datetime,
+  array20_weather,
+  array20_icon,
+  array20_temp,
+  array20_feels_like,
+  array20_humidity,
+  //Array22 data
+  array22_datetime,
+  array22_weather,
+  array22_icon,
+  array22_temp,
+  array22_feels_like,
+  array22_humidity,
   //Array24 data
   array24_datetime,
   array24_weather,
@@ -32,22 +94,64 @@ export function getHtml(
   array24_temp,
   array24_feels_like,
   array24_humidity,
+  //Array26 data
+  array26_datetime,
+  array26_weather,
+  array26_icon,
+  array26_temp,
+  array26_feels_like,
+  array26_humidity,
+  //Array24 data
+  array28_datetime,
+  array28_weather,
+  array28_icon,
+  array28_temp,
+  array28_feels_like,
+  array28_humidity,
+  //Array30 data
+  array30_datetime,
+  array30_weather,
+  array30_icon,
+  array30_temp,
+  array30_feels_like,
+  array30_humidity,
   //Array32 data
   array32_datetime,
   array32_weather,
   array32_icon,
   array32_temp,
   array32_feels_like,
-  array32_humidity
+  array32_humidity,
+  //Array34 data
+  array34_datetime,
+  array34_weather,
+  array34_icon,
+  array34_temp,
+  array34_feels_like,
+  array34_humidity,
+  //Array30 data
+  array36_datetime,
+  array36_weather,
+  array36_icon,
+  array36_temp,
+  array36_feels_like,
+  array36_humidity,
+  //Array38 data
+  array38_datetime,
+  array38_weather,
+  array38_icon,
+  array38_temp,
+  array38_feels_like,
+  array38_humidity
 ) {
   return `<div class = "current-weather">
   
-  <h2>${location}, ${country} weather for the next 3 hours</h2>
+  <h2>${location}, ${country}</h2>
   
   <button class = "wf-now">
-  
   <h3>${array0_datetime.toDateString()}</h3>
-       <p><strong>${array0_datetime.toLocaleTimeString()}</strong></p>
+  <h3>Current weather in 3 hour intervals</h3>
+       <p><strong>Until ${array0_datetime.toLocaleTimeString()}</strong></p>
   <h4>${array0_temp}°</h4>
   <div class = "wf-0">
 
@@ -61,15 +165,48 @@ export function getHtml(
         </div>
         </button>
         </div>
-        
     
         <div class = "wf">
-<h4>Scroll accross for this weeks weather forecast</h4>
+<h4>Scroll across for this weeks weather forecast</h4>
 <ul class = "slider">
+
+<li>
+        <button class = "wf-2">
+        <h3>${array2_datetime.toDateString()}</h3>
+        <p><strong>Until ${array2_datetime.toLocaleTimeString()}</strong></p>
+        <h4>${array2_temp}°</h4>
+        <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array2_icon}.png">
+        <h4>${array2_weather}</h4>
+        <p>Feels like ${array2_feels_like}°</p>
+        <p>Humidity ${array2_humidity}%</p>
+        </button>
+        </li>
+        <li>
+        <button class = "wf-4">
+        <h3>${array4_datetime.toDateString()}</h3>
+        <p><strong>Until ${array4_datetime.toLocaleTimeString()}</strong></p>
+        <h4>${array4_temp}°</h4>
+        <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array4_icon}.png">
+        <h4>${array4_weather}</h4>
+        <p>Feels like ${array4_feels_like}°</p>
+        <p>Humidity ${array4_humidity}%</p>
+        </button>
+        </li>
+<li>
+        <button class = "wf-6">
+        <h3>${array6_datetime.toDateString()}</h3>
+        <p><strong>Until ${array6_datetime.toLocaleTimeString()}</strong></p>
+        <h4>${array6_temp}°</h4>
+        <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array6_icon}.png">
+        <h4>${array6_weather}</h4>
+        <p>Feels like ${array6_feels_like}°</p>
+        <p>Humidity ${array6_humidity}%</p>
+        </button>
+     </li> 
 <li>
         <button class = "wf-8">
         <h3>${array8_datetime.toDateString()}</h3>
-        <p><strong>${array8_datetime.toLocaleTimeString()}</strong></p>
+        <p><strong>Until ${array8_datetime.toLocaleTimeString()}</strong></p>
         <h4>${array8_temp}°</h4>
         <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array8_icon}.png">
         <h4>${array8_weather}</h4>
@@ -77,12 +214,44 @@ export function getHtml(
         <p>Humidity ${array8_humidity}%</p>
         </button>
         </li>
-       
-        
+        <li>
+        <button class = "wf-10">
+        <h3>${array10_datetime.toDateString()}</h3>
+        <p><strong>Until ${array10_datetime.toLocaleTimeString()}</strong></p>
+        <h4>${array10_temp}°</h4> 
+        <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array10_icon}.png">
+        <h4>${array10_weather}</h4> 
+        <p>Feels like ${array10_feels_like}°</p>
+        <p>Humidity ${array10_humidity}%</p>
+        </button>
+       </li>
+       <li>
+       <button class = "wf-12">
+       <h3>${array12_datetime.toDateString()}</h3>
+       <p><strong>Until ${array12_datetime.toLocaleTimeString()}</strong></p>
+       <h4>${array12_temp}°</h4> 
+       <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array12_icon}.png">
+       <h4>${array12_weather}</h4> 
+       <p>Feels like ${array12_feels_like}°</p>
+       <p>Humidity ${array12_humidity}%</p>
+       </button>
+      </li>
+      <li>
+      <button class = "wf-14">
+      <h3>${array14_datetime.toDateString()}</h3>
+      <p><strong>Until ${array14_datetime.toLocaleTimeString()}</strong></p>
+      <h4>${array14_temp}°</h4> 
+      <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array14_icon}.png">
+      <h4>${array14_weather}</h4> 
+      <p>Feels like ${array14_feels_like}°</p>
+      <p>Humidity ${array14_humidity}%</p>
+      </button>
+     </li>
+
        <li>
             <button class = "wf-16">
             <h3>${array16_datetime.toDateString()}</h3>
-            <p><strong>${array16_datetime.toLocaleTimeString()}</strong></p>
+            <p><strong>Until ${array16_datetime.toLocaleTimeString()}</strong></p>
             <h4>${array16_temp}°</h4> 
             <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array16_icon}.png">
             <h4>${array16_weather}</h4> 
@@ -90,11 +259,45 @@ export function getHtml(
             <p>Humidity ${array16_humidity}%</p>
             </button>
            </li>
+           <li>
+           <button class = "wf-18">
+           <h3>${array18_datetime.toDateString()}</h3>
+           <p><strong>Until ${array18_datetime.toLocaleTimeString()}</strong></p>
+           <h4>${array18_temp}°</h4> 
+           <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array18_icon}.png">
+           <h4>${array18_weather}</h4> 
+           <p>Feels like ${array18_feels_like}°</p>
+           <p>Humidity ${array18_humidity}%</p>
+           </button>
+          </li>
 
+          <li>
+          <button class = "wf-20">
+          <h3>${array20_datetime.toDateString()}</h3>
+          <p><strong>Until ${array20_datetime.toLocaleTimeString()}</strong></p>
+          <h4>${array20_temp}°</h4>
+          <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array20_icon}.png">
+          <h4>${array20_weather}</h4> 
+          <p>Feels like ${array20_feels_like}°</p>
+          <p>Humidity ${array20_humidity}%</p>
+          </button>
+        </li>
+
+        <li>
+          <button class = "wf-22">
+          <h3>${array22_datetime.toDateString()}</h3>
+          <p><strong>Until ${array22_datetime.toLocaleTimeString()}</strong></p>
+          <h4>${array22_temp}°</h4>
+          <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array22_icon}.png">
+          <h4>${array22_weather}</h4> 
+          <p>Feels like ${array22_feels_like}°</p>
+          <p>Humidity ${array22_humidity}%</p>
+          </button>
+        </li>
         <li>
             <button class = "wf-24">
             <h3>${array24_datetime.toDateString()}</h3>
-            <p><strong>${array24_datetime.toLocaleTimeString()}</strong></p>
+            <p><strong>Until ${array24_datetime.toLocaleTimeString()}</strong></p>
             <h4>${array24_temp}°</h4>
             <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array24_icon}.png">
             <h4>${array24_weather}</h4> 
@@ -103,15 +306,84 @@ export function getHtml(
             </button>
           </li>
 
+          <li>
+          <button class = "wf-26">
+          <h3>${array26_datetime.toDateString()}</h3>
+          <p><strong>Until ${array26_datetime.toLocaleTimeString()}</strong></p>
+          <h4>${array26_temp}°</h4>
+          <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array26_icon}.png">
+          <h4>${array26_weather}</h4> 
+          <p>Feels like ${array26_feels_like}°</p>
+          <p>Humidity ${array26_humidity}%</p>
+          </button>
+        </li>
+
+        <li>
+        <button class = "wf-28">
+        <h3>${array28_datetime.toDateString()}</h3>
+        <p><strong>Until ${array28_datetime.toLocaleTimeString()}</strong></p>
+        <h4>${array28_temp}°</h4>
+        <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array28_icon}.png">
+        <h4>${array28_weather}</h4> 
+        <p>Feels like ${array28_feels_like}°</p>
+        <p>Humidity ${array28_humidity}%</p>
+        </button>
+      </li>
+
+
+      <li>
+      <button class = "wf-30">
+      <h3>${array30_datetime.toDateString()}</h3>
+      <p><strong>Until ${array30_datetime.toLocaleTimeString()}</strong></p>
+      <h4>${array30_temp}°</h4>
+       <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array30_icon}.png">
+       <h4>${array30_weather}</h4>
+       <p>Feels like ${array30_feels_like}°</p>
+       <p>Humidity ${array30_humidity}%</p>
+      </button>
+      </li>
         <li>
             <button class = "wf-32">
             <h3>${array32_datetime.toDateString()}</h3>
-            <p><strong>${array32_datetime.toLocaleTimeString()}</strong></p>
+            <p><strong>Until ${array32_datetime.toLocaleTimeString()}</strong></p>
             <h4>${array32_temp}°</h4>
              <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array32_icon}.png">
              <h4>${array32_weather}</h4>
              <p>Feels like ${array32_feels_like}°</p>
              <p>Humidity ${array32_humidity}%</p>
+            </button>
+            </li>
+            <li>
+            <button class = "wf-34">
+            <h3>${array34_datetime.toDateString()}</h3>
+            <p><strong>Until ${array34_datetime.toLocaleTimeString()}</strong></p>
+            <h4>${array34_temp}°</h4>
+             <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array34_icon}.png">
+             <h4>${array34_weather}</h4>
+             <p>Feels like ${array34_feels_like}°</p>
+             <p>Humidity ${array34_humidity}%</p>
+            </button>
+            </li>
+            <li>
+            <button class = "wf-36">
+            <h3>${array36_datetime.toDateString()}</h3>
+            <p><strong>Until ${array36_datetime.toLocaleTimeString()}</strong></p>
+            <h4>${array36_temp}°</h4>
+             <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array36_icon}.png">
+             <h4>${array36_weather}</h4>
+             <p>Feels like ${array36_feels_like}°</p>
+             <p>Humidity ${array36_humidity}%</p>
+            </button>
+            </li>
+            <li>
+            <button class = "wf-38">
+            <h3>${array38_datetime.toDateString()}</h3>
+            <p><strong>Until ${array38_datetime.toLocaleTimeString()}</strong></p>
+            <h4>${array38_temp}°</h4>
+             <img class= "wf-icon" src = "https://openweathermap.org/img/wn/${array38_icon}.png">
+             <h4>${array38_weather}</h4>
+             <p>Feels like ${array38_feels_like}°</p>
+             <p>Humidity ${array38_humidity}%</p>
             </button>
             </li>
             </ul>
--- weather.js
+++ weather.js
@@ -30,6 +30,32 @@ async function getWeather(latitude, longitude) {
   const tempArray0 = data.list[0].main.temp;
   const feels_likeArray0 = data.list[0].main.feels_like;
   const humidityArray0 = data.list[0].main.humidity;
+  // Weather data [Array 2]
+  const dateTime2 = data.list[2].dt_txt;
+  const dateTimeArray2 = new Date(dateTime2);
+  const weatherArray2 = data.list[2].weather[0].description;
+  const iconArray2 = data.list[2].weather[0].icon;
+  const tempArray2 = data.list[2].main.temp;
+  const feels_likeArray2 = data.list[2].main.feels_like;
+  const humidityArray2 = data.list[2].main.humidity;
+
+  // Weather data [Array 4]
+  const dateTime4 = data.list[4].dt_txt;
+  const dateTimeArray4 = new Date(dateTime4);
+  const weatherArray4 = data.list[4].weather[0].description;
+  const iconArray4 = data.list[4].weather[0].icon;
+  const tempArray4 = data.list[4].main.temp;
+  const feels_likeArray4 = data.list[2].main.feels_like;
+  const humidityArray4 = data.list[4].main.humidity;
+
+  // Weather data [Array 6]
+  const dateTime6 = data.list[6].dt_txt;
+  const dateTimeArray6 = new Date(dateTime6);
+  const weatherArray6 = data.list[6].weather[0].description;
+  const iconArray6 = data.list[6].weather[0].icon;
+  const tempArray6 = data.list[6].main.temp;
+  const feels_likeArray6 = data.list[6].main.feels_like;
+  const humidityArray6 = data.list[6].main.humidity;
 
   // Weather data [Array 8]
   const dateTime8 = data.list[8].dt_txt;
@@ -40,6 +66,33 @@ async function getWeather(latitude, longitude) {
   const feels_likeArray8 = data.list[8].main.feels_like;
   const humidityArray8 = data.list[8].main.humidity;
 
+  // Weather data [Array 10]
+  const dateTime10 = data.list[10].dt_txt;
+  const dateTimeArray10 = new Date(dateTime10);
+  const weatherArray10 = data.list[10].weather[0].description;
+  const iconArray10 = data.list[10].weather[0].icon;
+  const tempArray10 = data.list[10].main.temp;
+  const feels_likeArray10 = data.list[10].main.feels_like;
+  const humidityArray10 = data.list[10].main.humidity;
+
+  // Weather data [Array 12]
+  const dateTime12 = data.list[12].dt_txt;
+  const dateTimeArray12 = new Date(dateTime12);
+  const weatherArray12 = data.list[12].weather[0].description;
+  const iconArray12 = data.list[12].weather[0].icon;
+  const tempArray12 = data.list[12].main.temp;
+  const feels_likeArray12 = data.list[12].main.feels_like;
+  const humidityArray12 = data.list[12].main.humidity;
+
+  // Weather data [Array 14]
+  const dateTime14 = data.list[14].dt_txt;
+  const dateTimeArray14 = new Date(dateTime14);
+  const weatherArray14 = data.list[14].weather[0].description;
+  const iconArray14 = data.list[14].weather[0].icon;
+  const tempArray14 = data.list[14].main.temp;
+  const feels_likeArray14 = data.list[14].main.feels_like;
+  const humidityArray14 = data.list[14].main.humidity;
+
   // Weather data [Array 16]
   const dateTime16 = data.list[16].dt_txt;
   const dateTimeArray16 = new Date(dateTime16);
@@ -48,6 +101,33 @@ async function getWeather(latitude, longitude) {
   const tempArray16 = data.list[16].main.temp;
   const feels_likeArray16 = data.list[16].main.feels_like;
   const humidityArray16 = data.list[16].main.humidity;
+
+  // Weather data [Array 18]
+  const dateTime18 = data.list[18].dt_txt;
+  const dateTimeArray18 = new Date(dateTime18);
+  const weatherArray18 = data.list[18].weather[0].description;
+  const iconArray18 = data.list[18].weather[0].icon;
+  const tempArray18 = data.list[18].main.temp;
+  const feels_likeArray18 = data.list[18].main.feels_like;
+  const humidityArray18 = data.list[18].main.humidity;
+
+  // Weather data [Array 20]
+  const dateTime20 = data.list[20].dt_txt;
+  const dateTimeArray20 = new Date(dateTime20);
+  const weatherArray20 = data.list[20].weather[0].description;
+  const iconArray20 = data.list[20].weather[0].icon;
+  const tempArray20 = data.list[20].main.temp;
+  const feels_likeArray20 = data.list[20].main.feels_like;
+  const humidityArray20 = data.list[20].main.humidity;
+
+  // Weather data [Array 22]
+  const dateTime22 = data.list[22].dt_txt;
+  const dateTimeArray22 = new Date(dateTime22);
+  const weatherArray22 = data.list[22].weather[0].description;
+  const iconArray22 = data.list[22].weather[0].icon;
+  const tempArray22 = data.list[22].main.temp;
+  const feels_likeArray22 = data.list[22].main.feels_like;
+  const humidityArray22 = data.list[22].main.humidity;
 
   // Weather data [Array 24]
   const dateTime24 = data.list[24].dt_txt;
@@ -58,6 +138,33 @@ async function getWeather(latitude, longitude) {
   const feels_likeArray24 = data.list[24].main.feels_like;
   const humidityArray24 = data.list[24].main.humidity;
 
+  // Weather data [Array 26]
+  const dateTime26 = data.list[26].dt_txt;
+  const dateTimeArray26 = new Date(dateTime26);
+  const weatherArray26 = data.list[26].weather[0].description;
+  const iconArray26 = data.list[26].weather[0].icon;
+  const tempArray26 = data.list[26].main.temp;
+  const feels_likeArray26 = data.list[26].main.feels_like;
+  const humidityArray26 = data.list[26].main.humidity;
+
+  // Weather data [Array 28]
+  const dateTime28 = data.list[28].dt_txt;
+  const dateTimeArray28 = new Date(dateTime28);
+  const weatherArray28 = data.list[28].weather[0].description;
+  const iconArray28 = data.list[28].weather[0].icon;
+  const tempArray28 = data.list[28].main.temp;
+  const feels_likeArray28 = data.list[28].main.feels_like;
+  const humidityArray28 = data.list[28].main.humidity;
+
+  // Weather data [Array 30]
+  const dateTime30 = data.list[30].dt_txt;
+  const dateTimeArray30 = new Date(dateTime30);
+  const weatherArray30 = data.list[30].weather[0].description;
+  const iconArray30 = data.list[30].weather[0].icon;
+  const tempArray30 = data.list[30].main.temp;
+  const feels_likeArray30 = data.list[30].main.feels_like;
+  const humidityArray30 = data.list[30].main.humidity;
+
   // Weather data [Array 32]
   const dateTime32 = data.list[32].dt_txt;
   const dateTimeArray32 = new Date(dateTime32);
@@ -66,6 +173,33 @@ async function getWeather(latitude, longitude) {
   const tempArray32 = data.list[32].main.temp;
   const feels_likeArray32 = data.list[32].main.feels_like;
   const humidityArray32 = data.list[32].main.humidity;
+
+  // Weather data [Array 34]
+  const dateTime34 = data.list[34].dt_txt;
+  const dateTimeArray34 = new Date(dateTime34);
+  const weatherArray34 = data.list[34].weather[0].description;
+  const iconArray34 = data.list[34].weather[0].icon;
+  const tempArray34 = data.list[34].main.temp;
+  const feels_likeArray34 = data.list[34].main.feels_like;
+  const humidityArray34 = data.list[34].main.humidity;
+
+  // Weather data [Array 36]
+  const dateTime36 = data.list[36].dt_txt;
+  const dateTimeArray36 = new Date(dateTime36);
+  const weatherArray36 = data.list[36].weather[0].description;
+  const iconArray36 = data.list[36].weather[0].icon;
+  const tempArray36 = data.list[36].main.temp;
+  const feels_likeArray36 = data.list[36].main.feels_like;
+  const humidityArray36 = data.list[36].main.humidity;
+
+  // Weather data [Array 38]
+  const dateTime38 = data.list[38].dt_txt;
+  const dateTimeArray38 = new Date(dateTime38);
+  const weatherArray38 = data.list[38].weather[0].description;
+  const iconArray38 = data.list[38].weather[0].icon;
+  const tempArray38 = data.list[38].main.temp;
+  const feels_likeArray38 = data.list[38].main.feels_like;
+  const humidityArray38 = data.list[38].main.humidity;
 
   rootRef.innerHTML = getHtml(
     printCountry,
@@ -81,6 +215,30 @@ async function getWeather(latitude, longitude) {
     feels_likeArray0,
     humidityArray0,
 
+    //Array2 data
+    dateTimeArray2,
+    weatherArray2,
+    iconArray2,
+    tempArray2,
+    feels_likeArray2,
+    humidityArray2,
+
+    //Array4 data
+    dateTimeArray4,
+    weatherArray4,
+    iconArray4,
+    tempArray4,
+    feels_likeArray4,
+    humidityArray4,
+
+    //Array6 data
+    dateTimeArray6,
+    weatherArray6,
+    iconArray6,
+    tempArray6,
+    feels_likeArray6,
+    humidityArray6,
+
     //Array8 data
     dateTimeArray8,
     weatherArray8,
@@ -88,6 +246,30 @@ async function getWeather(latitude, longitude) {
     tempArray8,
     feels_likeArray8,
     humidityArray8,
+
+    //Array10 data
+    dateTimeArray10,
+    weatherArray10,
+    iconArray10,
+    tempArray10,
+    feels_likeArray10,
+    humidityArray10,
+
+    //Array12 data
+    dateTimeArray12,
+    weatherArray12,
+    iconArray12,
+    tempArray12,
+    feels_likeArray12,
+    humidityArray12,
+
+    //Array14 data
+    dateTimeArray14,
+    weatherArray14,
+    iconArray14,
+    tempArray14,
+    feels_likeArray14,
+    humidityArray14,
 
     //Array16 data
     dateTimeArray16,
@@ -97,6 +279,30 @@ async function getWeather(latitude, longitude) {
     feels_likeArray16,
     humidityArray16,
 
+    //Array18 data
+    dateTimeArray18,
+    weatherArray18,
+    iconArray18,
+    tempArray18,
+    feels_likeArray18,
+    humidityArray18,
+
+    //Array20 data
+    dateTimeArray20,
+    weatherArray20,
+    iconArray20,
+    tempArray20,
+    feels_likeArray20,
+    humidityArray20,
+
+    //Array22 data
+    dateTimeArray22,
+    weatherArray22,
+    iconArray22,
+    tempArray22,
+    feels_likeArray22,
+    humidityArray22,
+
     //Array24 data
     dateTimeArray24,
     weatherArray24,
@@ -105,13 +311,61 @@ async function getWeather(latitude, longitude) {
     feels_likeArray24,
     humidityArray24,
 
+    //Array26 data
+    dateTimeArray26,
+    weatherArray26,
+    iconArray26,
+    tempArray26,
+    feels_likeArray26,
+    humidityArray26,
+
+    //Array28 data
+    dateTimeArray28,
+    weatherArray28,
+    iconArray28,
+    tempArray28,
+    feels_likeArray28,
+    humidityArray28,
+
+    //Array30 data
+    dateTimeArray30,
+    weatherArray30,
+    iconArray30,
+    tempArray30,
+    feels_likeArray30,
+    humidityArray30,
+
     //Array32 data
     dateTimeArray32,
     weatherArray32,
     iconArray32,
     tempArray32,
     feels_likeArray32,
-    humidityArray32
+    humidityArray32,
+
+    //Array34 data
+    dateTimeArray34,
+    weatherArray34,
+    iconArray34,
+    tempArray34,
+    feels_likeArray34,
+    humidityArray34,
+
+    //Array36 data
+    dateTimeArray36,
+    weatherArray36,
+    iconArray36,
+    tempArray36,
+    feels_likeArray36,
+    humidityArray36,
+
+    //Array38 data
+    dateTimeArray38,
+    weatherArray38,
+    iconArray38,
+    tempArray38,
+    feels_likeArray38,
+    humidityArray38
   );
 }
 
